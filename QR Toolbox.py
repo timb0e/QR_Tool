16d60e0ff830a92c97ef6a9c029fd0525599b89c
timb0e <49237705+timb0e@users.noreply.github.com>
2019-08-15 16:28:09 -0400

    Update 1.1.1
    Added Sharepoint support. QR code images are now generated with text identifier.

--- QR Toolbox.py
+++ QR Toolbox.py
@@ -1,6 +1,6 @@
 # Name: QR Toolbox v2a
 # Description: The QR Toolbox is a suite a tools for creating and reading QR codes. See the About screen for more information
-# Author(s): Code integration, minor enhancements, & platform development - Timothy Boe boe.timothy@epa.gov; qrcode - Lincoln Loop info@lincolnloop.com; pyzbar - Lawrence Hudson quicklizard@googlemail.com; OpenCV code - Adrian Rosebrock https://www.pyimagesearch.com/author/adrian/; 
+# Author(s): Code integration, minor enhancements, & platform development - Timothy Boe boe.timothy@epa.gov; qrcode - Lincoln Loop info@lincolnloop.com; pyzbar - Lawrence Hudson quicklizard@googlemail.com; OpenCV code - Adrian Rosebrock https://www.pyimagesearch.com/author/adrian/;
 # Contact: Timothy Boe boe.timothy@epa.gov
 # Requirements: Python 3, pyzbar, imutils, opencv-python, qrcode[pil]
 
@@ -11,18 +11,29 @@ from pyzbar import pyzbar
 from tkinter import *
 from tkinter import filedialog
 from datetime import timedelta
+from office365.runtime.auth.authentication_context import AuthenticationContext
+from office365.sharepoint.client_context import ClientContext
+from office365.sharepoint.file import File
+from office365.sharepoint.file_creation_information import FileCreationInformation
+from time import strftime
+from PIL import Image, ImageDraw, ImageFont
+from settings import settings
 
+listTitle = "QR Timestamps"
+qrfolder = "QRCodes"
+bkcsvfolder = "HXWTEST"
+qrbatchfile = "names.csv"
+relative_url = "/sites/Emergency%20Response/EOCIncident/EOC%20Documents/QRCodes/names.csv"
 # load variables
-# set share folder default, assign system ID, and wait time
-share_folder = "None"
+
 system_id = os.environ['COMPUTERNAME']
 t_value = timedelta(seconds = 10)
 
 # display landing screen
 
-print("     _/_/      _/_/_/        _/_/_/_/_/                    _/  _/                   ")      
+print("     _/_/      _/_/_/        _/_/_/_/_/                    _/  _/                   ")
 print("  _/    _/    _/    _/          _/      _/_/      _/_/    _/  _/_/_/      _/_/    _/    _/")
-print(" _/  _/_/    _/_/_/            _/    _/    _/  _/    _/  _/  _/    _/  _/    _/    _/_/  ") 
+print(" _/  _/_/    _/_/_/            _/    _/    _/  _/    _/  _/  _/    _/  _/    _/    _/_/  ")
 print("_/    _/    _/    _/          _/    _/    _/  _/    _/  _/  _/    _/  _/    _/  _/    _/  ")
 print(" _/_/  _/  _/    _/          _/      _/_/      _/_/    _/  _/_/_/      _/_/    _/    _/ \n")
 print("QR Toolbox v2a \n")
@@ -30,13 +41,17 @@ print("The QR Toolbox is a suite a tools for creating and reading QR codes.\n")
 print("USEPA Homeland Security Research Program \n")
 print("System ID: " + system_id + "\n")
 
+context_auth = AuthenticationContext(url=settings['url'])
+context_auth.acquire_token_for_app(client_id=settings['client_id'], client_secret=settings['client_secret'])
+ctx = ClientContext(settings['url'], context_auth)
+
 def video():
     # construct the argument parser and parse the arguments
-    ap = argparse.ArgumentParser()  
+    ap = argparse.ArgumentParser()
     ap.add_argument("-o", "--output", type=str, default="barcodes.csv",
             help="path to output CSV file containing barcodes")
 #    ap.add_argument("-o1", "--output2", type=str, default=files_name,
-#            help="path to output CSV file containing barcodes")     
+#            help="path to output CSV file containing barcodes")
     args = vars(ap.parse_args())
     # initialize time and date and make filename friendly
     time_header = str(datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S'))
@@ -52,17 +67,13 @@ def video():
     # open the output CSV file for writing and initialize the set of
     # barcodes found thus far
     csv = open(args["output"], "w")
-    if os.path.exists(share_folder):
-        csv2 = open(os.path.join(share_folder,"QRT" + "-" + system_id+ "_" + time_header + ".csv"), "w")
-    else:
-        print("Alert: Share folder not established or is unavailable. Files will be saved to the working directory")
-        print("")
+
 
     # time track variables. These are used to keep track of QR codes as they enter the screen
     found = []
     found_time = []
     found_status = []
-
+    ctxAuth = AuthenticationContext(url=settings['url'])
     # loop over the frames from the video stream
     while True:
             # grab the frame from the threaded video stream and resize it to
@@ -72,7 +83,7 @@ def video():
 
             # find the barcodes in the frame and decode each of the barcodes
             barcodes = pyzbar.decode(frame)
-
+            timestr = strftime("%m/%d/%Y %H:%M")
             # loop over the detected barcodes
             for barcode in barcodes:
                     # extract the bounding box location of the barcode and draw
@@ -97,28 +108,27 @@ def video():
                             csv.write("{},{},{},{}\n".format(system_id,datetime.datetime.now(),
                                     barcodeData,"IN"))
                             csv.flush()
-                            #csv2 writes to share folder location
-                            if os.path.exists(share_folder):
-                                csv2.write("{},{},{},{}\n".format(system_id,datetime.datetime.now(),
-                                    barcodeData,"IN"))
-                                csv2.flush()
-                            else:
-                                pass
+                            contentstr = "{},{},{},{}\n".format(system_id,timestr, barcodeData,"IN")
+
+                            create_list_item(ctx,contentstr)
+                            fname ="QRT" + "-" + system_id+ "_" + time_header + ".csv"
+                            upload_file(ctx,contentstr,fname,bkcsvfolder)
+
                             found.append(barcodeData)
                             found_time.append(datetime.datetime.now())
                             found_status.append("IN")
                             sys.stdout.write('\a')
                             sys.stdout.flush()
                             print(barcodeData + " checking IN at " + str(datetime.datetime.now()) + " at location: " + system_id)
-                            
+
                     # if barcode information is found...
-                    
+
                     elif barcodeData in found:
                         time_check = datetime.datetime.now() - found_time[found.index(barcodeData)]
                         status_check = found_status[found.index(barcodeData)]
-                        
+
                         # if time exceeds wait period and user is checked in then check them out
-                        
+
                         if time_check > t_value and status_check == "IN":
                             index_loc = found.index(barcodeData)
                             found_status[index_loc] = "OUT"
@@ -126,17 +136,15 @@ def video():
                             csv.write("{},{},{},{},{}\n".format(system_id,datetime.datetime.now(),
                                     barcodeData,"OUT",time_check))
                             csv.flush()
-                            #csv2 writes to share folder location
-                            # check and make sure share folder exists
-                            if os.path.exists(share_folder):
-                                csv2.write("{},{},{},{},{}\n".format(system_id,datetime.datetime.now(),
-                                    barcodeData,"OUT",time_check))
-                                csv2.flush()
-                            else:
-                                pass
+                            contentstr = "{},{},{},{},{}\n".format(system_id,timestr, barcodeData,"OUT",time_check)
+
+                            create_list_item(ctx,contentstr)
+                            fname ="QRT" + "-" + system_id+ "_" + time_header + ".csv"
+                            upload_file(ctx,contentstr,fname,bkcsvfolder)
+
                             sys.stdout.write('\a')
                             sys.stdout.flush()
-                            print(barcodeData + " checking OUT at " + str(datetime.datetime.now()) + " at location: " + system_id + " for duration of " + str(time_check))                       
+                            print(barcodeData + " checking OUT at " + str(datetime.datetime.now()) + " at location: " + system_id + " for duration of " + str(time_check))
                         # if found and check-in time is less than the specified wait time then wait
                         elif time_check < t_value and status_check == "OUT":
                             pass
@@ -147,11 +155,11 @@ def video():
                             del found[index_loc]
                     else:
                         print("Something happened... error")
-                            
+
             # show the output frame
             cv2.imshow("QR Toolbox", frame)
             key = cv2.waitKey(1) & 0xFF
-     
+
             # if the `q` key was pressed, break from the loop
             if key == ord("q"):
                     break
@@ -161,20 +169,56 @@ def video():
     csv.close()
     cv2.destroyAllWindows()
     vs.stop()
+def create_list_item(ctx,content):
+    print("Create list item example...")
+    list_object = ctx.web.lists.get_by_title(listTitle)
+    values = content.split(",")
+    sid = values[0]
+    tstr = values[1]
+    barstr = values[2]
+    status = values[3]
+
+    item_properties = {'__metadata': {'type': 'SP.Data.QR_x0020_TimestampsListItem'}, 'Title': barstr,'QR_x0020_Terminal':sid,'Time_x0020_Stamp':tstr,'Info': status}
+    item = list_object.add_item(item_properties)
+    ctx.execute_query()
+    print("List item '{0}' has been created.".format(item.properties["Title"]))
+
+def upload_file(context,file_content,filename,subfolder):
+    list_title = "EOC Documents"
+    library = context.web.lists.get_by_title(list_title)
+
+    filecontext = library.context
+    info = FileCreationInformation()
+    info.content = file_content
+    info.url = filename
+    info.overwrite = True
+    ##upload file to subfolder 'eoctest'
+    target_file = library.root_folder.folders.get_by_url(subfolder).files.add(info)
+
+    filecontext.execute_query()
 
 def qr_batch():
 
     print("")
-    print("The batch QR code tool is used to automatically create multiple QR codes by referencing a .csv file. The csv file must be stored in the tools origin folder, named 'names.csv', and consist of two columns 'first' & 'last'. The 'first' and 'last' columns should be populated with participant's first and last names. The tool will automatically create QR codes for each participant's full name and save each QR image to the tools origin folder. \n")
+    print("The batch QR code tool is used to automatically create multiple QR codes by referencing a .csv file. The tool will automatically create QR codes for each participant's email address and save each QR image to the SharePoint site. \n")
     input("Press Enter to Continue \n")
     # this code creates a batch of QR codes from a csv file stored in the local directory
     # QR code image size and input filename can be modified below
-    with open('names.csv') as csvfile:
-        fieldnames= ["first", "last"]
+    resp = File.open_binary(ctx, relative_url)
+    status = resp.status_code
+    
+    if status == 404:
+        print("The batch file '" + relative_url + "' doesn't exist. Please copy 'names.csv' to the sharepoint site.")
+        return False
+    
+    with open(qrbatchfile, 'wb') as output_file:
+        output_file.write(resp.content)
+
+    with open(qrbatchfile) as csvfile:
         reader= csv.reader(csvfile)
 
         for row in reader:
-            labeldata = row[0] + " " + row[1]
+            labeldata = row[0]
 
             qr = qrcode.QRCode(
             version=1,
@@ -185,14 +229,35 @@ def qr_batch():
             qr.add_data(labeldata)
             qr.make(fit=True)
             print("Creating QR code: " +labeldata)
-            img = qr.make_image()
-            img.save(labeldata+".jpg") #assigns data as label name
 
-        print("Success! \n")
+            # draw QR image
+            
+            img = qr.make_image()
+            qrfile =labeldata+".jpg"
+            img.save(qrfile)
+
+            # open QR image and add qr data as name
+            
+            img = Image.open(qrfile)
+            draw = ImageDraw.Draw(img)
+            font = ImageFont.truetype("arial", 18)
+            color = 0
+            draw.text((37,10),labeldata, font=font, fill=color)
+            img.save(qrfile)
+
+            # upload file
+            
+            with open(qrfile, 'rb') as content_file:
+                file_content = content_file.read()
+            upload_file(ctx,file_content,qrfile,qrfolder)
+            os.remove(qrfile)
+            
+    os.remove(qrbatchfile)
+    print("Success! \n")
 
 def qr_single():
 
-    print("")    
+    print("")
     print("Enter text to generate a single QR code and press Enter. The resulting QR image will be saved in the tool's origin folder. \n")
     custom_labeldata = input("QR Text: ")
     print("Creating QR code...")
@@ -209,14 +274,31 @@ def qr_single():
     qr.make(fit=True)
 
     img = qr.make_image()
-    img.save(custom_labeldata+".jpg")
+    filename = custom_labeldata+".jpg"
+    img.save(filename)
+
+    # open QR image and add qr data as name
+            
+    img = Image.open(filename)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("arial", 18)
+    color = 0
+    draw.text((37,10),custom_labeldata, font=font, fill=color)
+    img.save(filename)
+
+    # upload file
+    
+    with open(filename, 'rb') as content_file:
+        file_content = content_file.read()
+    upload_file(ctx,file_content,filename,qrfolder)
+    os.remove(filename)
 
     print("Success! \n")
 
 def about():
     # displays the about screen
     print("\n")
-    print("QR Toolbox v2a \n")    
+    print("QR Toolbox v2a \n")
     print("About: The QR Toolbox is a suite a tools for creating and reading QR codes. The toolbox is platform agnostic, lightweight, open source, and written in pure Python. This toolbox may be used to track resources, serve as a check-in capability for personnel, or customized to meet other operational needs. \n")
     print("Version: 2.0a \n")
     print("Credits: The QR Toolbox consists of a number of python packages, namely: \n qrcode - Lincoln Loop info@lincolnloop.com; \n pyzbar - Lawrence Hudson quicklizard@googlemail.com; \n OpenCV code - Adrian Rosebrock https://www.pyimagesearch.com/author/adrian/; \n Code integration, minor enhancements, & platform development - Timothy Boe boe.timothy@epa.gov \n")
@@ -264,12 +346,13 @@ def cons():
 # main menu
 
 while True:
+
     print("==============|  MENU  |===============")
     print("A. QR Reader")
     print("B. QR Creator - Batch")
     print("C. QR Creator - Single")
     print("D. Establish Share Folder")
-    print("E. Consolidate Records")    
+    print("E. Consolidate Records")
     print("F. About/Credits")
     print("G. Exit \n")
     choice = input("Enter your selection: ")
@@ -284,7 +367,7 @@ while True:
     elif choice.lower() == 'e':
        cons()
     elif choice.lower() == 'f':
-       about()        
+       about()
     elif choice.lower() == 'g':
        break
     else:
